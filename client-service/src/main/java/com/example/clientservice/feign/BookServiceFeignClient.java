c6e30c4ba5f7778407a53d83f6639801ac610461
Sergey Fomenko <artem.work749@gmail.com>
2024-02-26 17:19:25 +0300

    Add logging

--- client-service/src/main/java/com/example/clientservice/feign/BookServiceFeignClient.java
+++ client-service/src/main/java/com/example/clientservice/feign/BookServiceFeignClient.java
@@ -1,23 +1,29 @@
 package com.example.clientservice.feign;
 
 import com.example.clientservice.model.Book;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "book-service", fallback = BookServiceFeignClient.Fallback.class)
+@FeignClient(name = "book-service")
+@CircuitBreaker(name = "book-service", fallbackMethod = "fallback")
 public interface BookServiceFeignClient {
     @GetMapping("/api/books")
     List<Book> getBooks();
 
-    @Component
-    class Fallback implements BookServiceFeignClient {
-        @Override
-        public List<Book> getBooks() {
-            return Collections.emptyList();
-        }
+    default List<Book> fallback(Throwable ex) {
+        final Logger logger = LogManager.getLogger(BookServiceFeignClient.class);
+        logger.warn("CUSTOM LOG - " + ex.getMessage());
+        return List.of(Book.builder()
+                        .id("fallback-book-id")
+                        .title("fallback-book-title")
+                        .description("fallback-book-description")
+                        .imageLink("fallback-book-image-link")
+                .build());
+
     }
 }
